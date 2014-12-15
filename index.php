48cb81254b7b7c5252f339c85d25caf4c076233e
Mike Rickson <mikerickson14@gmail.com>
2014-12-15 15:47:21 +1300

    a file you need

--- index.php
+++ index.php
@@ -3,5 +3,6 @@ echo '<h1>This is my GIT test!</h1>';
 echo 'Master Branch - yes, that worked! <br />another conflict more text<br />';
 echo '<h4>Introducing a change to Master...</h4>';
 echo '<p>A New Factoid Here - now it\'s up to date!</p>';
+require('you_need_this.php');
 exit;
 ?>
