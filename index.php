61ee2137e2f8e8bd37497fb3fc2c7d6455e70d41
Mike Rickson <mikerickson14@gmail.com>
2014-12-15 14:06:51 +1300

    up to date

--- index.php
+++ index.php
@@ -2,6 +2,6 @@
 echo 'This is my GIT test!';
 echo 'Master Branch - yes, that worked! <br />another conflict more text';
 echo 'Introducing a change to Master...';
-echo 'A New Fact Here!';
+echo 'A New Fact Here - now it\'s up to date.!';
 exit;
 ?>
