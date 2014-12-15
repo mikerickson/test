0c9892bb5b16ab16f6abc4347e7abc63685bb990
Mike Rickson <mikerickson14@gmail.com>
2014-12-15 13:39:30 +1300

    more features

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 echo 'this is my git test!';
-echo 'MASTER - yes that worked !!another conflict more texts';
+echo 'MASTER - yes that worked !!another conflict more text';
+echo 'a new feature here';
 exit;
 ?>
